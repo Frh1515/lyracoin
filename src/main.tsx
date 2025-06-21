b3dd629aef3871f0cb0ad305cfd1d1a4845541fd
Frh1515 <shmsmhmd356@gmail.com>
2025-06-21 18:53:57 +0300

    Fix wallet logo display by creating tonconnect-manifest.json and updating main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TonConnectUIProvider manifestUrl="https://www.lyracoine.com/tonconnect-manifest.json">
+    <TonConnectUIProvider manifestUrl="https://unique-melomakarona-b3adfe.netlify.app/tonconnect-manifest.json">
       <App />
     </TonConnectUIProvider>
   </StrictMode>
